be2cfe725896f92dd9218a4462e974d80b8f7a64
sergey <sergey@psv73.net>
2020-02-28 11:32:40 +0100

    Start

--- src/main/java/net/psv73/insurance/controller/InsuranceController.java
+++ src/main/java/net/psv73/insurance/controller/InsuranceController.java
@@ -3,34 +3,37 @@ package net.psv73.insurance.controller;
 import net.psv73.insurance.model.Insurance;
 import net.psv73.insurance.repository.InsuranceRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.servlet.ModelAndView;
 
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
 
-@RestController
+@Controller
 @RequestMapping("insurance")
 public class InsuranceController {
+
+//    @Value("@(app.main.title)")
+//    private String title = "";
 
     @Autowired
     private InsuranceRepository insuranceRepository;
 
-    @GetMapping
-    public ModelAndView main(Map<String, Object> model) {
+    @GetMapping()
+    public String main(Map<String, Object> model) {
 
         List<Insurance> insurances = insuranceRepository.findAll();
 
         model.put("insurances", insurances);
 
-        return new ModelAndView("index", model);
+        return "main";
     }
 
     @GetMapping("{id}")
     public String getOne(@PathVariable String id, Map<String, Object> model) {
 
-        Optional<Insurance> insurance = insuranceRepository.findById(Integer.parseInt(id));
+        Optional<Insurance> insurance = insuranceRepository.findById(Long.parseLong(id));
 
         model.put("insurances", insurance);
 
--- src/main/java/net/psv73/insurance/repository/InsuranceRepository.java
+++ src/main/java/net/psv73/insurance/repository/InsuranceRepository.java
@@ -4,8 +4,11 @@ import net.psv73.insurance.model.Insurance;
 import org.springframework.data.repository.CrudRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface InsuranceRepository extends CrudRepository<Insurance, Integer> {
 
     List<Insurance> findAll();
+
+    Optional<Insurance> findById(long parseLong);
 }
