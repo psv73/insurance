69a043381bff0263014dcdb72f2d55e2c595910c
Sergii Ponomarenko <sergey@psv73.net>
2020-03-03 07:08:47 +0100

    REST service finished

--- src/main/java/net/psv73/insurance/model/Rate.java
+++ src/main/java/net/psv73/insurance/model/Rate.java
@@ -5,58 +5,39 @@ import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.stereotype.Component;
 
 @ToString
 @EqualsAndHashCode
-@Component
+@Getter
+@Setter
+@Configuration
 @PropertySource("classpath:application.properties")
 @ConfigurationProperties(prefix = "app")
 public class Rate {
 
-    @Getter
-    @Setter
     private float basicDay;
 
-    @Getter
-    @Setter
     private float basicYear;
 
-    @Getter
-    @Setter
     private float extendedDay;
 
-    @Getter
-    @Setter
     private float extendedYear;
 
-    @Getter
-    @Setter
     private float extraDay;
 
-    @Getter
-    @Setter
     private float extraYear;
 
-    @Getter
-    @Setter
     private float cancellationDay;
 
-    @Getter
-    @Setter
     private float cancellationYear;
 
-    @Getter
-    @Setter
     private float sportDay;
 
-    @Getter
-    @Setter
     private float sportYear;
 
-    @Getter
-    @Setter
     private String name;
 
     public Rate() {
