41db4d604da1ce2beb5b58730e524ffaa9807e47
sergey <sergey@psv73.net>
2020-03-03 12:52:38 +0100

    V3

--- src/main/java/net/psv73/insurance/model/Rate.java
+++ src/main/java/net/psv73/insurance/model/Rate.java
@@ -5,7 +5,6 @@ import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
 import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.stereotype.Component;
 
 @Getter
