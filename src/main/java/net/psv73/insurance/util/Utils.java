25247e78f3036b40a3e693ca741d5eaeac5bc7b1
sergey <sergey@psv73.net>
2020-03-04 10:52:19 +0100

    Ms

--- src/main/java/net/psv73/insurance/util/Utils.java
+++ src/main/java/net/psv73/insurance/util/Utils.java
@@ -1,42 +1,16 @@
 package net.psv73.insurance.util;
 
-import net.psv73.insurance.model.*;
+import net.psv73.insurance.model.Insurance;
+import net.psv73.insurance.model.Person;
+import net.psv73.insurance.model.Plan;
+import net.psv73.insurance.model.Type;
 
-import java.io.File;
-import java.io.FileReader;
-import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
-import java.util.Properties;
 
 public class Utils {
 
-    public static float getProperty(String key) {
-
-        float property;
-        FileReader fileReader = null;
-        File file = new File("src/main/resources/config.properties");
-
-        Properties properties = new Properties();
-
-        try {
-            fileReader = new FileReader(file);
-            properties.load(fileReader);
-
-        } catch(IOException io) {
-            io.printStackTrace();
-        } finally {
-            if (fileReader != null) {
-
-            }
-        }
-
-        property = Float.parseFloat(properties.getProperty(key));
-
-        return property;
-    }
-
-    public static Map<String, Object> editData(Insurance insurance) {
+     public static Map<String, Object> editData(Insurance insurance) {
 
         Map<String, Object> data = new HashMap<>();
 
